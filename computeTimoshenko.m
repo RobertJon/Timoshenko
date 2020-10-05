baf78735ae2f7eae2a8193cfdbd1faa1ae2e4ac6
RobertJon <robjonss@kth.se>
2020-10-05 12:52:55 +0200

    Modified

--- computeTimoshenko.m
+++ computeTimoshenko.m
@@ -1,7 +1,8 @@
 % This file is used to find the BC's for a Timoshenko beam (Sandwich)
 
-function beam = computeTimoshenko(model,plotcurves)
+function beam = computeTimoshenko(model)
 
+plotcurves = model.plotcurves;
 
 names = fieldnames(model);
 for i=1:numel(names)
@@ -21,12 +22,24 @@ switch loadcase
         Tx(x>xP) = -P*xP;
         
         %Bending moment distribution
-        Mx = P*(1-xP)*x;
-        Mx(x>xP) = P*xP*(L-x(x>xP));
+        Mx = P*b*x;
+        Mx(x>xP) = P*a*(L-x(x>xP));
+
         
         %Twist angle distribution
-        t = P*L^3*b/(6*D)*( (1-b^2)/L -3*x.^2./(L^3) ) + P*b/S;
-        t(x>xP) = P*L^3*a/(6*D)*( (a^2-1)/L + 3*(L-x(x>xP)).^2./(L^3)) -P*a/S;
+        
+        tb(x<xP) = -b*P*((b^2-1)*L^2 + 3*x(x<xP).^2)/(6*D);
+        tb(x==xP) = 0;
+        tb(x>xP) = a*P*((a^2+2)*L^2 -6*L*x(x>xP) + 3*x(x>xP).^2)/(6*D);   
+        
+        ts(x<xP) = P*b/S;
+        ts(x==xP) = 0;
+        ts(x>xP) = -P*a/S;
+        
+        ttot=tb+ts;
+        
+        %t = P*L^3*b/(6*D)*( (1-b^2)/L -3*x.^2./(L^3) ) + P*b/S;
+        %t(x>xP) = P*L^3*a/(6*D)*((a^2-1)/L + 3*(L-x(x>xP)).^2./(L^3)) -P*a/S;
         
         %Deflections due to bending
         wb = P*L^3*b/(6*D)*((1-b^2)*(x./L)-(x./L).^3);
@@ -35,10 +48,13 @@ switch loadcase
         %Deflections due to shear (Timoshenko)
         ws = P*b*x/S;
         ws(x>xP) = P*a*(L-x(x>xP))/S; 
+        %ws = Tx*b/S;
         
         %Total deflection
         w = wb+ws;
+        %w = ws;
         w(x>xP) = wb(x>xP) + ws(x>xP);
+        
         
     case 'simple_dist'
         %Assuming that the input load is in Newton, the distribution is
@@ -56,18 +72,18 @@ switch loadcase
         Mx = RL*x-0.5*q*x.^2;
         
         %Twist angle distribution
-        t = q*L^4/(24*D)*( 4*x.^3/(L^4) -6.*x.^2/(L^3) + 1/L) + q/(2*S)*(L-2.*x);
+        tb = q*L^4/(24*D)*( 4*x.^3/(L^4) -6.*x.^2/(L^3) + 1/L);
+        ts = q/(2*S)*(L-2.*x);
+        ttot = tb + ts;
         
         %Deflections due to bending
-        wb = q*L^4/(24*D)*((x/L).^4 -2*(x/L).^3 + (x/L)) + q/(2*S).*(L*x-x.^2);
-        
+        wb = q*L^4/(24*D)*((x/L).^4 -2*(x/L).^3 + (x/L));% + q/(2*S).*(L*x-x.^2);
         %Deflections due to shear (Timoshenko)
         ws = q/(2*S)*(L*x-x.^2);
         
         %Total deflection
         w = wb + ws;
         
-
     case 'cantilever_pt'
         %Force input is in Newtons, located at the free end of the
         %cantilever beam.
@@ -84,8 +100,8 @@ switch loadcase
         Mx = Tx.*(xP*L-x);
         
         %Twist angle distribution
-        t = P*L^2/(2*D)*(b^2-(eta-a).^2);
-        t(eta>a) = P*L^2/(2*D)*b^2;
+        ttot = P*L^2/(2*D)*(b^2-(eta-a).^2);
+        ttot(eta>a) = P*L^2/(2*D)*b^2;
       
         %Deflections due to bending
         wb = P*L^3/(6*D)*(-b^3 + 3*b^2*(1-eta));
@@ -100,59 +116,85 @@ switch loadcase
         w = wb + ws;
 
     case 'cantilever_dist'
-        
-        eta = x/L;
-        
+
         %Force input in Newton. 
         Q = P/L;
         
         %Shear force distribution
-        Tx = Q*x;
+        %Tx = Q*x;
+        Tx = Q.*(x-L);
         
         %Moment distribution
-        Mx = -Q*0.5*x.^2;
+        %Mx = -Q*0.5*x.^2;
+        Mx = -Q*L/(2*L)*(L-x).^2;
         
-        %Twist angle distribution
-        t = Q*L^2/(6*D)*(1-eta.^3);
-        t = flip(t);
+        %Twist angle distribution due to bending
+        tb = Q*L^4/(24*D)*( 4*x.^3/(L^4) -12*x.^2/(L^3) +12*x/(L^2) );
+        
+        %Twist angle distribution due to shear
+        ts = Tx./S;
+        
+        %Total angle distribution
+        ttot = tb + ts;
         
         %Deflections due to bending
-        wb = Q*L^3/(24*D)*(eta.^4-4*eta+3);
+        wb = Q*L^4/(24*D)*( (x/L).^4 -4*(x/L).^3 +6*(x/L).^2 );
+        
+        %wb = Q*L^3/(24*D)*(eta.^4-4*eta+3);
         
         %Deflections due to shear
-        ws = Tx.*x/S;
+        ws = Q*x./(2*S).*(2*L-x);
+        %ws = Tx.*x/S;
         
         %Total deflection
         w = wb + ws;
-        w = flip(w);
+        %w = flip(w);
         
     otherwise
         warning('case not defined')
 end
     
-if plotcurves
-    subplot(2,2,1)
-    plot(x,w,'b','linewidth',1)
-    xlabel('x'),ylabel('w')
-    grid on
-    subplot(2,2,2)
-    plot(x,t,'b','linewidth',1)
-    xlabel('x'),ylabel('\theta [rad]')
-    grid on
-    subplot(2,2,3)
-    plot(x,Mx,'b','linewidth',1)
-    xlabel('x'),ylabel('M')
-    grid on
-    subplot(2,2,4)
-    plot(x,Tx,'b','linewidth',1)
-    xlabel('x'),ylabel('Q')
-    grid on
+switch plotcurves
+    case 'on'
+        subplot(2,2,1)
+        cla reset
+        hold on
+        plot(x,w,'b','linewidth',1)
+        plot(x,wb,'r--','linewidth',1)
+        plot(x,ws,'b:','linewidth',1)
+        xlabel('x'),ylabel('w')
+        grid on
+        title('Displacement')
+        legend('Total','Bending','Shear')
+        subplot(2,2,2)
+        cla reset
+        hold on
+        plot(x,ttot,'b','linewidth',1)
+        plot(x,tb,'r--','linewidth',1)
+        plot(x,ts,'b:','linewidth',1)
+        xlabel('x'),ylabel('\theta [rad]')
+        grid on
+        title('Rotation')
+        legend('Total','Bending','Shear')
+        subplot(2,2,3)
+        cla reset
+        plot(x,Mx,'b','linewidth',1)
+        xlabel('x'),ylabel('M')
+        grid on
+        title('Bending moment')
+        subplot(2,2,4)
+        axis tight
+        plot(x,Tx,'b','linewidth',1)
+        xlabel('x'),ylabel('Q')
+        grid on
+        title('Shear force')
+        
 end
 
 if exist('f')
-    beam=struct('x',x,'w',w,'t',t,'M',Mx,'Q',Tx,'f',f);
+    beam=struct('x',x,'w',w,'t',ttot,'M',Mx,'Q',Tx,'f',f);
 else
-    beam=struct('x',x,'w',w,'t',t,'M',Mx,'Q',Tx);
+    beam=struct('x',x,'w',w,'t',ttot,'M',Mx,'Q',Tx);
 end
 
 end
